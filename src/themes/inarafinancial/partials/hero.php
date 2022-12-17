cd1c76a8c4b9ce8794f0aaf0a350f4c2483d5f4c
patrick <patrick@progeny.tech>
2022-12-17 10:37:48 -0500

    responsive adjustments

--- src/themes/inarafinancial/partials/hero.php
+++ src/themes/inarafinancial/partials/hero.php
@@ -1,23 +1,23 @@
 <div class="hero hero--<?= $args['modifier'];?>">
     <?= wp_get_attachment_image(get_field('hero')['image'], 'hero');?>    
     <div class="hero__container container">
-        <div class="hero__content">
-
-            <?php if(!empty(get_field('hero')['icon'])){?>                
-                <div class="hero__icon"><?= wp_get_attachment_image(get_field('hero')['icon']);?></div>
-            <?php }?>
-
-            <?php if(!empty(get_field('hero')['content'])){
-                echo get_field('hero')['content'];
-            }?>
-
-            <?php if(!empty(get_field('hero')['book_button'])){?>
-                <ul class="hero__buttons">
-                    <li><a class="button button--book" href="<?= get_field('hero')['book_button']['link'];?>"><?= get_field('hero')['book_button']['label'];?></a></li>
-                    <li><a class="button button--secondary" href="<?= get_field('hero')['learn_button']['link'];?>"><?= get_field('hero')['learn_button']['label'];?></a></li>
-                </ul>
-            <?php }?>
-
-        </div>
+        <?php if(!empty(get_field('hero')['content'])){?>
+            <div class="hero__content">
+    
+                <?php if(!empty(get_field('hero')['icon'])){?>                
+                    <div class="hero__icon"><?= wp_get_attachment_image(get_field('hero')['icon']);?></div>
+                <?php }?>
+    
+                <?= get_field('hero')['content'];?>
+    
+                <?php if(!empty(get_field('hero')['book_button'])){?>
+                    <ul class="hero__buttons">
+                        <li><a class="button button--book" href="<?= get_field('hero')['book_button']['link'];?>"><?= get_field('hero')['book_button']['label'];?></a></li>
+                        <li><a class="button button--secondary" href="<?= get_field('hero')['learn_button']['link'];?>"><?= get_field('hero')['learn_button']['label'];?></a></li>
+                    </ul>
+                <?php }?>
+    
+            </div>
+        <?php }?>
     </div>
 </div>
