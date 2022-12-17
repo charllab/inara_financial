7f0197bc3467d3c4bbef3cf45e3a76832ad0aa2f
patrick <patrick@progeny.tech>
2022-12-17 10:51:58 -0500

    next level profits page

---
+++ src/themes/inarafinancial/page-next-level-profits.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<?php get_template_part( 'partials/hero', null, array(
+    'modifier'=>'profits'
+) );?>
+<section class="inner-catch-section">
+    <div class="inner-catch-section__container container">
+        <div class="inner-catch-section__content">
+            <?= get_field('catch_section')['content'];?>
+            <a class="button button--book" href="<?= get_field('catch_section')['book_button']['link'];?>"><?= get_field('catch_section')['book_button']['label'];?></a>
+        </div>
+    </div>
+</section>
+<?php get_template_part( 'partials/about-section', null, array(
+    'modifier'=>'profits'
+) );?>
+<?php get_template_part( 'partials/blueprint-section' );?>
+<?php get_template_part( 'partials/book-banner-section' );?>
+<?php get_template_part( 'partials/ideal-section' );?>
+<?php get_template_part( 'partials/testimonial-section' );?>
+<?php get_template_part( 'partials/profits-banner-section' );?>
+<?php get_footer();
